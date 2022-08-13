6cf0f2b8d7951952ba3f15e67e10525374ba751a
matsuri-gaoka <repetitive789++@gmail.com>
2022-08-13 14:25:46 +0900

    runBlockingの引数を別に書くようにした

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
 import kotlinx.coroutines.*
 
-fun main() = runBlocking { // this: CoroutineScope
-    launch { // launch a new coroutine and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
+fun main() {
+    val x: suspend CoroutineScope.()-> Unit = {
+        launch {
+            delay(1000L)
+            println("World!")
+        }
+        println("Hello")
     }
-    println("Hello") // main coroutine continues while a previous one is delayed
+    runBlocking(block=x)
+
+//    runBlocking { // this: CoroutineScope
+//        launch { // launch a new coroutine and continue
+//            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+//            println("World!") // print after delay
+//        }
+//        println("Hello") // main coroutine continues while a previous one is delayed
+//    }
+}
+
+fun test(){
+
 }
